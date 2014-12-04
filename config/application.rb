47240930b72285e5efd91b613772415bc15be8d0
Jeremy Wadsack <jeremy.wadsack@gmail.com>
2014-12-04 14:32:51 -0800

    Add file system cache store

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module CacheMarshalTest
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Use a file cache store
+    config.cache_store = :file_store, File.join(Rails.root, 'tmp/cache')
   end
 end
